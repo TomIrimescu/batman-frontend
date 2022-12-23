154ea4d59f18f47dcf20f489a5cdbc188f03368a
Tom Irimescu <tom@irimescu.com>
2022-12-23 08:58:53 -0500

    Re-order listbox

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import { gql } from "graphql-request";
 
+import { gql } from "graphql-request";
 import { client } from "~/lib/graphql-client";
 
 import { Fragment, useState } from 'react';
@@ -896,6 +896,65 @@ export default function Index() {
         </div>
       </div>
 
+      {/* listbox */}
+      <div className="w-6/12 mx-auto mb-14">
+        <Listbox value={selected} onChange={setSelected}>
+          {({ open }) => (
+            <>
+              <Listbox.Label className="block text-sm font-medium text-gray-700">Assigned to</Listbox.Label>
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+                  <span className="block truncate">{selected.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {people.map((person) => (
+                      <Listbox.Option
+                        key={person.id}
+                        className={({ active }) =>
+                          classNames(
+                            active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                            'relative cursor-default select-none py-2 pl-8 pr-4'
+                          )
+                        }
+                        value={person}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                              {person.name}
+                            </span>
+                            {selected ? (
+                              <span
+                                className={classNames(
+                                  active ? 'text-white' : 'text-indigo-600',
+                                  'absolute inset-y-0 left-0 flex items-center pl-1.5'
+                                )}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </>
+          )}
+        </Listbox>
+      </div>      
+
       {/* headless ui tabs */}
       <div className="w-6/12 mx-auto mb-8">
         <Tab.Group>
@@ -958,65 +1017,6 @@ export default function Index() {
             ))}
           </Tab.Panels>
         </Tab.Group>
-      </div>
-
-      {/* listbox */}
-      <div className="w-6/12 mx-auto mb-80">
-        <Listbox value={selected} onChange={setSelected}>
-          {({ open }) => (
-            <>
-              <Listbox.Label className="block text-sm font-medium text-gray-700">Assigned to</Listbox.Label>
-              <div className="relative mt-1">
-                <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
-                  <span className="block truncate">{selected.name}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  </span>
-                </Listbox.Button>
-                <Transition
-                  show={open}
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {people.map((person) => (
-                      <Listbox.Option
-                        key={person.id}
-                        className={({ active }) =>
-                          classNames(
-                            active ? 'text-white bg-indigo-600' : 'text-gray-900',
-                            'relative cursor-default select-none py-2 pl-8 pr-4'
-                          )
-                        }
-                        value={person}
-                      >
-                        {({ selected, active }) => (
-                          <>
-                            <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                              {person.name}
-                            </span>
-                            {selected ? (
-                              <span
-                                className={classNames(
-                                  active ? 'text-white' : 'text-indigo-600',
-                                  'absolute inset-y-0 left-0 flex items-center pl-1.5'
-                                )}
-                              >
-                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </>
-          )}
-        </Listbox>
       </div>
 
     </>
