e9d1484b1315a269f23a0055213cdcf53c8dceed
Tom Irimescu <tom@irimescu.com>
2022-12-23 08:48:29 -0500

    Add @headlessui tabs

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ import { gql } from "graphql-request";
 import { client } from "~/lib/graphql-client";
 
 import { Fragment, useState } from 'react';
-import { Dialog, Disclosure, Listbox, Menu, Transition } from '@headlessui/react';
+import { Tab, Dialog, Disclosure, Listbox, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, CheckCircleIcon, CheckIcon, ChevronUpDownIcon, ExclamationTriangleIcon, InformationCircleIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 export interface Post {
@@ -66,7 +66,58 @@ function classNames(...classes: string[]) {
 export default function Index() {
   const [modal, setModal] = useState(false);
   const [drawer, setDrawer] = useState(false);
-  const [selected, setSelected] = useState(people[3]);   
+  const [selected, setSelected] = useState(people[3]);
+  let [categories] = useState({
+    Recent: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    Popular: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+    Trending: [
+      {
+        id: 1,
+        title: 'Ask Me Anything: 10 answers to your questions about coffee',
+        date: '2d ago',
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: '4d ago',
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+  })
+
   let { posts } = useLoaderData();
 
   return (
@@ -823,7 +874,7 @@ export default function Index() {
         </div>
       </div>
 
-      <div className="rounded-md bg-green-50 p-4 max-w-md mx-auto mb-8">
+      <div className="rounded-md bg-green-50 p-4 max-w-md mx-auto mb-14">
         <div className="flex">
           <div className="flex-shrink-0">
             <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
@@ -843,6 +894,70 @@ export default function Index() {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* headless ui tabs */}
+      <div className="w-6/12 mx-auto mb-8">
+        <Tab.Group>
+          <Tab.List className="flex space-x-1 rounded-xl bg-indigo-600 p-1">
+            {Object.keys(categories).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                    selected
+                      ? 'bg-white shadow'
+                      : 'text-blue-100 hover:bg-white/[0.24] hover:text-white'
+                  )
+                }
+              >
+                {category}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="mt-2">
+            {Object.values(categories).map((posts, idx) => (
+              <Tab.Panel
+                key={idx}
+                className={classNames(
+                  'rounded-xl bg-white p-3',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                )}
+              >
+                <ul>
+                  {posts.map((post) => (
+                    <li
+                      key={post.id}
+                      className="relative rounded-md p-3 hover:bg-gray-100"
+                    >
+                      <h3 className="text-sm font-medium leading-5">
+                        {post.title}
+                      </h3>
+
+                      <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                        <li>{post.date}</li>
+                        <li>&middot;</li>
+                        <li>{post.commentCount} comments</li>
+                        <li>&middot;</li>
+                        <li>{post.shareCount} shares</li>
+                      </ul>
+
+                      <a
+                        href="#"
+                        className={classNames(
+                          'absolute inset-0 rounded-md',
+                          'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
+                        )}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
       </div>
 
       {/* listbox */}
