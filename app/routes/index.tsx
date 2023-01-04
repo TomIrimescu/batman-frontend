ed110656357d1cd1581ecfbd963cbd4fefc076b0
Tom Irimescu <tom@irimescu.com>
2023-01-04 14:52:38 -0500

    Add @headlessui switch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,7 +6,7 @@ import { client } from "~/lib/graphql-client";
 
 import { Fragment, useState } from 'react';
 
-import { Tab, Dialog, Disclosure, Listbox, Menu, Transition } from '@headlessui/react';
+import { Tab, Dialog, Disclosure, Listbox, Menu, Transition, Switch } from '@headlessui/react';
 import { Bars3Icon, BellIcon, CheckCircleIcon, CheckIcon, ChevronUpDownIcon, ExclamationTriangleIcon, InformationCircleIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 export interface Post {
@@ -68,6 +68,7 @@ export default function Index() {
   const [modal, setModal] = useState(false);
   const [drawer, setDrawer] = useState(false);
   const [selected, setSelected] = useState(people[3]);
+  const [enabled, setEnabled] = useState(false)  
   let [categories] = useState({
     Recent: [
       {
@@ -955,7 +956,7 @@ export default function Index() {
       </div>      
 
       {/* headless ui tabs */}
-      <div className="w-6/12 mx-auto mb-8">
+      <div className="w-6/12 mx-auto mb-14">
         <Tab.Group>
           <Tab.List className="flex space-x-1 rounded-xl bg-indigo-600 p-1">
             {Object.keys(categories).map((category) => (
@@ -1016,6 +1017,23 @@ export default function Index() {
             ))}
           </Tab.Panels>
         </Tab.Group>
+      </div>
+
+      {/* headless ui switch/toggle */}
+      <div className="w-1/12 mx-auto mb-8">
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
+            relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        >
+          <span className="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+              pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+          />
+        </Switch>
       </div>
 
     </>
