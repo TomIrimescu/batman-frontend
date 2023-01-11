bd7edb7e5319f75d35401fd212873195705b23cc
Tom Irimescu <tom@irimescu.com>
2023-01-11 16:51:45 -0500

    Change profile image

--- app/components/MainNavigation.tsx
+++ app/components/MainNavigation.tsx
@@ -19,7 +19,7 @@ export default function MainNavigation() {
   return (
     <>
       {/* tailwind ui top menu */}
-      <Disclosure as="nav" className="bg-gray-800 fixed top-0 w-full z-20">
+      <Disclosure as="nav" className="bg-gray-800 fixed top-0 w-full z-10">
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -80,8 +80,8 @@ export default function MainNavigation() {
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
+                          src="./../images/tirimescu.png"
+                          alt="Tom Irimescu"
                         />
                       </Menu.Button>
                     </div>
