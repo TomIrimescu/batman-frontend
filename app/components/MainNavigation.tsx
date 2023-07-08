3dd9c201f18d030cfd6f580fac668ae66866a973
Tom Irimescu <tom@irimescu.com>
2023-07-08 15:28:56 -0400

    Modify menus

--- app/components/MainNavigation.tsx
+++ app/components/MainNavigation.tsx
@@ -118,6 +118,15 @@ export default function MainNavigation() {
         <NavigationMenu.Item>
           <NavigationMenu.Link
             className='text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
+            href='/about-us'
+          >
+            About Us
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Link
+            className='text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
             href='/people'
           >
             People
